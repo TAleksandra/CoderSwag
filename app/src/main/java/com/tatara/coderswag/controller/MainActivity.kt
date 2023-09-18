fba6ec96024c6f1e5adcd87df9ef492663dd2523
TAleksandra <tatara.ola@gmail.com>
2023-09-19 00:48:19 +0200

    lesson 02

--- app/src/main/java/com/tatara/coderswag/controller/MainActivity.kt
+++ app/src/main/java/com/tatara/coderswag/controller/MainActivity.kt
@@ -2,11 +2,23 @@ package com.tatara.coderswag.controller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
 import com.tatara.coderswag.R
+import com.tatara.coderswag.model.Category
+import com.tatara.coderswag.services.DataService
 
 class MainActivity : AppCompatActivity() {
+    lateinit var adapter: ArrayAdapter<Category>
+    lateinit var categoryListView: ListView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        adapter = ArrayAdapter(this,
+            android.R.layout.simple_list_item_1,
+            DataService.categories)
+        categoryListView = findViewById(R.id.categoryListView)
+        categoryListView.adapter = adapter
     }
 }
